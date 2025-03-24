f781b38beeeab7d5755fef85786b443a38642d9f
qinyue <qinyue@liaoshen.com>
2025-03-24 10:51:10 +0800

    @required 修饰的方法，必须实现该方法，如果不实现 会出现警告，可以运行
    //@optional 如果不实现 也不会警告

--- StudyIosDemo/Pen.h
+++ StudyIosDemo/Pen.h
@@ -10,8 +10,12 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @protocol Pen <NSObject>
+//@required 修饰的方法，必须实现该方法，如果不实现 会出现警告，可以运行
+@required
 - (void)doHomeWork;
 - (void)doHomeWork:(NSString*) bookName;
+//@optional 如果不实现 也不会警告
+@optional
 - (void)writeBook;
 @end
 
